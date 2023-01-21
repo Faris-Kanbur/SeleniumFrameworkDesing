0ce301c623dd7a9ce510652974ee88b83b732484
Faris Kanbur <60715227+Faris-Kanbur@users.noreply.github.com>
2023-01-21 14:03:49 +0100

    edit

--- src/main/java/rahulshettyacademy/AbstractComponents/AbstractComponent.java
+++ src/main/java/rahulshettyacademy/AbstractComponents/AbstractComponent.java
@@ -1,4 +1,4 @@
-package fariskanburacademy.AbstractComponents;
+package rahulshettyacademy.AbstractComponents;
 
 import java.time.Duration;
 
@@ -10,8 +10,8 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import fariskanburacademy.pageobjects.CartPage;
-import fariskanburacademy.pageobjects.OrderPage;
+import rahulshettyacademy.pageobjects.CartPage;
+import rahulshettyacademy.pageobjects.OrderPage;
 
 public class AbstractComponent {
 	
